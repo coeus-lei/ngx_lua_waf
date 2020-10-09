d2a63b798762d739c9d7e8735b66113c70350943
Simmon <42054370+coeus-lei@users.noreply.github.com>
2020-10-09 20:19:57 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,18 @@
-RulePath = "/usr/local/nginx/waf/wafconf/"
+RulePath = "/usr/local/nginx/conf/wafconf/"
 attacklog = "on"
 --args post  url  user-agent..日志文件开关
 logtofile = "on"
 --ccdeny or hackipdeny日志记录开关-
 denycclog="on"
 --日志路径
-logdir = "/home/wwwlogs/waf/"
+logdir = "/data/wwwlogs/waf/"
 UrlDeny="on"
 Redirect="on"
 CookieMatch="on"
 postMatch="on" 
 whiteModule="on" 
 black_fileExt={"php","jsp"}
-ipWhitelist={"139.159.194.220","127.0.0.1","119.8.104.73","159.138.40.51","159.138.40.162","159.138.10.120","119.8.103.214","159.138.32.40","159.138.40.64","159.138.48.77"}
+ipWhitelist={"127.0.0.1"}
 ipBlocklist={"1.0.0.1"}
 --hackrate超过10次/1800秒,限制访问86400秒。
 hackipdeny="on"
